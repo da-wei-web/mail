ba0dee25882eeaa3a07b7e1f0ccc36945c75c451
大卫 <1499782633@qq.com>
2020-07-02 17:07:09 +0800

    封装顶部导航

--- src/views/work/Work.vue
+++ src/views/work/Work.vue
@@ -1,13 +1,49 @@
 <template>
-  <h2>职位</h2>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <template v-slot:center>
+        <h2>工作</h2>
+      </template>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
+import NavBar from 'components/common/navbar/NavBar.vue';
+import home from 'network/home';
+
 export default {
-  name: 'Work'
+  name: 'Work',
+  components: {
+    NavBar
+  },
+  data(){
+    return {
+      banners: [],
+      dKeywords: [],
+      keywords: [],
+      recommends: []
+    }
+  },
+  // 发送网络请求
+  created() {
+    home.getHomeMultidata().then(res => {
+      console.log(res)
+      this.banners = res.data.banner.list;
+      this.dKeywords = res.data.dKeyword.list;
+      this.keywords = res.data.keywords.list;
+      this.recommends = res.data.recommend.list;
+    });
+  }
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+  @import "assets/css/base.less";
+  .home-nav {
+    background-color: @tint-color;
+    color: @bgc;
+  }
+</style>>
 
 </style>
