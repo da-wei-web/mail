ea80ef2a65fc88898743b3f905ef8f5e9bc06a5e
大卫 <1499782633@qq.com>
2020-07-10 12:04:40 +0800

    创建详情页面级组件

---
+++ src/views/details/Detail.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="detail">
+    <!-- <h2>{{ iid }}</h2> -->
+    <!-- 头部导航 -->
+    <detail-nav-bar />
+  </div>
+</template>
+
+<script>
+  import DetailNavBar from './detailnavbar/DetailNavBar';
+
+  // 网络请求
+  import sHome from 'network/home';
+  export default {
+    name: 'Detail',
+    data() {
+      return {
+        // 要和后端要求的字段名一致
+        iid: null
+      }
+    },
+    components: {
+      DetailNavBar
+    },
+    created() {
+      // 获取该个体的iid, 并保存起来
+      this.iid = this.$route.params.iid;
+      // console.log(this.iid)
+      // 调用toGoodsDetail方法发送网络请求
+      this.toGoodsDetail(this.iid);
+      
+    },
+    methods: {
+      /*
+       * 网络请求详情页数据
+       */
+      toGoodsDetail(id) {
+        sHome.toGoodsDetail(id).then(res => {
+          console.log(res)
+        })
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ src/views/details/detailnavbar/DetailNavBar.vue
@@ -0,0 +1,61 @@
+<template>
+  <nav-bar>
+    <template v-slot:left>
+      <span class="iconfont icon-fanhui" @click="backTo"></span>
+    </template>
+    <template v-slot:center>
+      <ul class="detail-nav-bar">
+        <li v-for="(item, index) in ['hah', 'b2b', 'c33', 'sss']"
+            :key="index"
+            :class="{active: currentIndex === index}"
+            @click="handleClick(index)">
+            <span>{{ item }}</span>
+        </li>
+      </ul>
+    </template>
+  </nav-bar>
+</template>
+
+<script>
+  import NavBar from 'components/common/navbar/NavBar'; 
+  export default {
+    name: 'DetailNavBar',
+    data() {
+      return {
+        currentIndex: 0
+      }
+    },
+    components: {
+      NavBar
+    },
+    methods: {
+      // 菜单切换
+      handleClick(index) {
+        this.currentIndex = index;
+      },
+
+      // 返回
+      backTo() {
+        this.$router.push('/home');
+      }
+
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  @import 'assets/css/base.less';
+  .detail-nav-bar {
+    display: flex;
+    width: 100%;
+    height: 100%;
+    
+    > li {
+      flex: 1;
+    }
+
+    > .active {
+      color: @text-high-color;
+    }
+  }
+</style>
