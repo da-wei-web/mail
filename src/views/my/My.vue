eb81196c47d904208d9840a05b70b9939ee98bd0
大卫 <1499782633@qq.com>
2020-08-10 10:15:45 +0800

    个人中心登录页布局

--- src/views/my/My.vue
+++ src/views/my/My.vue
@@ -1,13 +1,38 @@
 <template>
-  <h2>我的</h2>
+  <div id="my">
+    <nav-bar class="my-nav">
+      <template v-slot:center>
+        <h2>个人中心</h2>
+      </template>
+    </nav-bar>
+    <avatar />
+    <login />
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'My'
-}
+  import NavBar from 'components/common/navbar/NavBar'
+
+  import Avatar from './childComs/Avatar';
+  import Login from './childComs/Login';
+  export default {
+    name: 'My',
+    components: {
+      NavBar,
+      Avatar,
+      Login 
+    }
+  }
 </script>
 
-<style>
+<style lang="less" scoped>
+  @import 'assets/css/base.less';
+  #my {
+    height: 100vh;
 
+    > .my-nav {
+      color: #fff;
+      background-color: @tint-color;
+    }
+  }
 </style>
---
+++ src/views/my/childComs/Avatar.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="avatar-container">
+    <img :src="avatar" alt="">
+    <span>{{ userName }}</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Avatar',
+    props: {
+      avatar: {
+        type: String,
+        default: 'https://s10.mogucdn.com/mlcdn/c45406/180926_7d5c521e0aa3h38786lkakebkjlh8_750x390.jpg'
+      },
+      userName: {
+        type: String,
+        default: '用户名'
+      }
+    }
+  }
+</script>
+
+<style lang="less" scoped>
+  @import 'assets/css/base.less';
+  .avatar-container {
+    width: 100%;
+    height: 2rem;
+    text-align: center;
+    background-color: #fff;
+
+    > img {
+      width: 1rem;
+      height: 1rem;
+      margin: .3rem 0 .1rem;
+      border: 1px solid @text-high-color;
+      border-radius: 50%;
+      background-color: #fff;
+    }
+
+    > span {
+      display: block;
+      padding: .1rem .08rem;
+      font-size: .14rem;
+      color: @tint-color;
+    }
+  }
+</style>
